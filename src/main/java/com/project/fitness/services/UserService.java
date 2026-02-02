99cd84e65d0ddd3dfc8474d200cf0d8a93aec9fd
Anuj <aanujmaurya8127@gmail.com>
2026-02-02 18:20:36 +0530

    Update user Service class

--- src/main/java/com/project/fitness/services/UserService.java
+++ src/main/java/com/project/fitness/services/UserService.java
@@ -1,4 +1,43 @@
 package com.project.fitness.services;
 
+import com.project.fitness.dto.RegisterRequest;
+import com.project.fitness.dto.UserResponse;
+import com.project.fitness.model.User;
+import com.project.fitness.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public UserResponse register(RegisterRequest request){
+        User user= User.builder()
+                .email(request.getEmail())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .password(request.getPassword())
+                .build();
+
+
+        User savedUser=userRepository.save(user);
+        return mapToResponse(savedUser);
+    }
+
+    private UserResponse mapToResponse(User savedUser) {
+        UserResponse response=new UserResponse();
+        response.setId(savedUser.getId());
+        response.setEmail(savedUser.getEmail());
+        response.setPassword(savedUser.getPassword());
+        response.setFirstName(savedUser.getFirstName());
+        response.setLastName(savedUser.getLastName());
+        response.setCreatedAt(savedUser.getCreatedAt());
+        response.setUpdatedAt(savedUser.getUpdatedAt());
+        return response;
+    }
 }
