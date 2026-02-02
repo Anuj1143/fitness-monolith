d7b085ad22927cc4a1ac352a4d2a83b31bc142ec
Anuj <aanujmaurya8127@gmail.com>
2026-02-02 18:18:37 +0530

    update recommendation entity class

--- src/main/java/com/project/fitness/model/Recommendation.java
+++ src/main/java/com/project/fitness/model/Recommendation.java
@@ -3,7 +3,9 @@ package com.project.fitness.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -30,7 +32,9 @@ public class Recommendation {
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "json")
     private List<String> safety;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
