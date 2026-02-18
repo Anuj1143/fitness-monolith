af08cc48964f8290a41b4b74d493761325522a13
Anuj <aanujmaurya8127@gmail.com>
2026-02-18 15:10:38 +0530

    Update Register request to handle register user

--- src/main/java/com/project/fitness/dto/RegisterRequest.java
+++ src/main/java/com/project/fitness/dto/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.project.fitness.dto;
 
+import com.project.fitness.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
     private String firstName;
     private String lastName;
+    private UserRole role;
+
 
 }
